41bb7e62bff48c019370f0753c42bed6b62acdd4
Munajat Ibnu <widget.my.id@gmail.com>
2022-01-01 14:16:47 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,6 +55,7 @@ $route['main']                	   = 'Main';
 $route['menu']                	   = 'Menu';
 $route['backupdb']                 = 'Backupdb';
 $route['iklan']                    = 'iklan';
+$route['sosmed']                   = 'sosmed';
 $route['portal']                   = 'Portal/index';
 $route['logout']                   = 'Logout';
 $route['rubrik/(:any)']	           = 'rubrik/index';
