bdd75efaf62b7724829a800d6b2eb9b147478d6b
Munajat Ibnu <widget.my.id@gmail.com>
2022-01-01 22:35:58 +0700

    Update Search.php

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -13,45 +13,62 @@
         
 		public function index()
 		{
-			$data['title'] = 'Pencarian | '.tag_key('site_title';
+			$data['title'] = 'Pencarian | '.tag_key('site_title');
 			$data['description'] = tag_key('site_desc');
 			$data['keywords'] = tag_key('site_desc');
 			$data['canonical']=base_url('search/');
 			$data['url_image'] = 'url_image';
+			$data['publisher']=sosmed_single('FB');
 			$data['json']=[
             "@context" => "https://schema.org",
             "@type" =>  "Organization",
             "name" =>  tag_key('site_name'),
-            "url" =>  "https://www.lenternews.tv",
+            "url" =>  tag_key('site_url'),
             "sameAs" => sosmed()
             ];
 			
+			if ($this->input->get('pencarian') !== FALSE) {
+				$conditions['search']['keywords'] = $this->input->get('s',true);
+				} else {
+				$conditions['search']['keywords'] = array();
+			}
+			// print_r($conditions);
 			$conditions['where'] = array(
 			'posting.tanggal <'=>mdate("%Y-%m-%d %H:%i:%s"),'`posting`.`publish`' => 'Y'
 			);
 			$conditions['returnType'] = 'count';
 			$totalRec = $this->model_data->getBlog($conditions);
 			// Pagination configuration 
-			$config['target']      = '#postList-tab-0';
+			$config['target']      = '#ResultCari';
 			$config['base_url']    = base_url('search/ajaxSearch');
 			$config['total_rows']  = $totalRec;
 			$config['per_page']    = $this->perPage;
 			$config['link_func']   = 'searchAll';
 			
 			// Initialize pagination library 
-			$this->ajax_pagemobile->initialize($config);
+			$this->paging_rubrik->initialize($config);
 			
 			// Get records 
 			$conditions = array(
 			'limit' => $this->perPage
 			);
+            
+			
+			if ($this->input->get('pencarian') !== FALSE) {
+				$conditions['search']['keywords'] = $this->input->get('s',true);
+				} else {
+				$conditions['search']['keywords'] = array();
+			}
+			
 			// $where2 = ['posting.tanggal <'=>mdate("%Y-%m-%d %H:%i:%s"),'`posting`.`publish`' => 'Y'];
 			$conditions['where'] = array(
 			'posting.tanggal <'=>mdate("%Y-%m-%d %H:%i:%s"),'`posting`.`publish`' => 'Y'
 			);
 			// print_r($conditions);
-			$data['populer'] = populer();
+			
+            unset($conditions['returnType']);
 			$data['posts']    = $this->model_data->getBlog($conditions);
+			$data['keywords']    = $this->input->get('s',true);
 			
 			$this->template->load(template().'/themes',template().'/search',$data);
 		}
@@ -69,48 +86,38 @@
             if (!empty($keywords)) {
                 $conditions['search']['keywords'] = $keywords;
 			}
-            $sortBy = $this->input->post('sortBy');
-            // if (!empty($sortBy)) {
-			// $conditions['search']['sortBy'] = $sortBy;
-			// }
-			
-			
-            if (!empty($sortBy) AND $sortBy!=0) {
-				$conditions['where'] = array(
-				'posting.id_cat' => $sortBy
-				);
-			}
+            
             // Get record count 
             $conditions['returnType'] = 'count';
             $totalRec = $this->model_data->getBlog($conditions);
             
             // Pagination configuration 
-            $config['target']      = '#postList-tab-'.$sortBy;
+            $config['target']      = '#ResultCari';
             $config['base_url']    = base_url('search/ajaxSearch');
             $config['total_rows']  = $totalRec;
             $config['per_page']    = $this->perPage;
             $config['link_func']   = 'searchAll';
             
             // Initialize pagination library 
-            $this->ajax_pagemobile->initialize($config);
+            $this->paging_rubrik->initialize($config);
             
             // Get records 
             $conditions['start'] = $offset;
             $conditions['limit'] = $this->perPage;
             
-			if (!empty($sortBy) AND $sortBy!=0) {
-				$conditions['where'] = array(
-				'posting.id_cat' => $sortBy
-				);
+			$keywords = $this->input->post('keywords');
+            if (!empty($keywords)) {
+                $conditions['search']['keywords'] = $keywords;
 			}
 			
             unset($conditions['returnType']);
 			
-			$data['tab'] = $sortBy;
             $data['posts'] = $this->model_data->getBlog($conditions);
+			$data['keywords'] = $keywords;
             
 			
             // Load the data list view 
             $this->load->view(template() . '/ajax-search', $data, false);
 		}
+		
 	}
