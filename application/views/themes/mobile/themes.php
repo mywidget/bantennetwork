086a0c96a9f009d4288cca4cbb3ddfe93f0c21f3
mywidget <widget.my.id@gmail.com>
2022-02-04 20:02:53 +0700

    Update themes.php

--- application/views/themes/mobile/themes.php
+++ application/views/themes/mobile/themes.php
@@ -6,6 +6,13 @@ prefix="og: https://ogp.me/ns#" > <![endif]-->
 <!--[if gt IE 8]><!--> <html lang="id-ID"
 prefix="og: https://ogp.me/ns#" > <!--<![endif]-->
 	<head>
+	<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-MB82R7S');</script>
+		<!-- End Google Tag Manager -->
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- All in One SEO Pro 4.1.5.1 -->
@@ -35,14 +42,15 @@ prefix="og: https://ogp.me/ns#" > <!--<![endif]-->
 			<?php echo json_encode($json,JSON_UNESCAPED_SLASHES); ?>
 		</script>
 		<meta name="google-site-verification" content="O3eO02JzK5_PbGU81ViihOtF35SWqpD0l3IWCyU9Wa8" />
+		
 		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-L2N7LVLVVF"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-EZG0SLG9M8"></script>
 		<script>
 			window.dataLayer = window.dataLayer || [];
 			function gtag(){dataLayer.push(arguments);}
 			gtag('js', new Date());
 			
-			gtag('config', 'G-L2N7LVLVVF');
+			gtag('config', 'G-EZG0SLG9M8');
 		</script>
 		
 		<link rel='dns-prefetch' href='//fonts.googleapis.com' />
@@ -256,5 +264,9 @@ prefix="og: https://ogp.me/ns#" > <!--<![endif]-->
 			
 			})(jQuery);
 		</script>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MB82R7S"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 	</body>
 </html>	
